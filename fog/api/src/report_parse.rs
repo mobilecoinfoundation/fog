7f0a8e9fa1fdfa15239fbe084c92dc79ed2488bf
Chris Beck <beck.ct@gmail.com>
2021-08-03 20:12:45 -0600

    simplify fog report parse code to avoid sgx-types dependency (thanks james)

--- fog/api/src/report_parse.rs
+++ fog/api/src/report_parse.rs
@@ -4,7 +4,6 @@ use core::convert::TryFrom;
 use displaydoc::Display;
 use mc_attest_core::{VerificationReport, VerificationReportData, VerifyError};
 use mc_crypto_keys::{CompressedRistrettoPublic, KeyError};
-use mc_sgx_types::sgx_report_data_t;
 use mc_util_encodings::Error as EncodingError;
 
 /// Helper function which extracts an unvalidated ingress pubkey from fog ingest
@@ -25,11 +24,12 @@ pub fn try_extract_unvalidated_ingress_pubkey_from_fog_report(
     // This extracts the user-data attached to the report, which is a thin wrapper
     // around [u8; 64]
     let report_data = verification_report_data.quote.report_body()?.report_data();
-    // Unwrap the wrapper
-    let report_data = sgx_report_data_t::from(report_data);
     // The second half of this is the data we care about, per the fog-ingest-enclave
     // identity implementation. These 32 bytes should be Ristretto.
-    Ok(CompressedRistrettoPublic::try_from(&report_data.d[32..64])?)
+    let report_data_bytes: &[u8] = report_data.as_ref();
+    Ok(CompressedRistrettoPublic::try_from(
+        &report_data_bytes[32..64],
+    )?)
 }
 
 /// An error which occurs when parsing an AVR
